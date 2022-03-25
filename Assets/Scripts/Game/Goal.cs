2d51f0deac68f3901f6be5212b28f8ed93c6596d
AlexeiKonev <alexkonev777@gmail.com>
2022-03-25 19:58:30 +0300

    по нарисованой линии идёт движение кубиков

---
+++ Assets/Scripts/Game/Goal.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public GameObject myBoy;
+
+    public bool isBoyHere;
+    private StageManager stageManager;
+    private Color color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBoyHere = false;
+        stageManager = FindObjectOfType<StageManager>();
+        color = this.GetComponent<MeshRenderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsBoyHere()
+    {
+        if(isBoyHere)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == myBoy)
+        {
+            isBoyHere = true;
+            this.GetComponent<MeshRenderer>().material.color = new Color(color.r + 0.5f, color.g + 0.5f, color.b + 0.5f);
+
+            if (stageManager != null)
+            {
+                stageManager.CheckEveryBoyInGoal();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == myBoy)
+        {
+            this.GetComponent<MeshRenderer>().material.color = color;
+            isBoyHere = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Game/StageManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    private Goal[] goals;
+    private int notClearNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goals = FindObjectsOfType<Goal>();
+        notClearNum = goals.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CheckEveryBoyInGoal()
+    {
+        for(int i = 0; i < goals.Length; i++)
+        {
+            if(goals[i].IsBoyHere() == false)
+            {
+                return;
+            }
+        }
+
+        ClearGame();
+    }
+
+    private void ClearGame()
+    {
+        BoyManager[] BoyManagers = FindObjectsOfType<BoyManager>();
+
+        for(int i = 0; i < BoyManagers.Length; i++)
+        {
+            BoyManagers[i].TurnOffCollider();
+        }
+    }
+}
